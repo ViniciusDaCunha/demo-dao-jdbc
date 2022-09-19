2bffa56ed7ba0bc084e386eaadaa1f0ce378dcb5
Vinicius da Cunha <dacunhavini@gmail.com>
2022-09-19 11:38:48 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,17 @@
+package model.dao;
+
+import db.DB;
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+/**responsavel por implementar os daos,
+	com operações estaticas para implementar os daos
+**/	
+	
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+	
+	
+}
+
